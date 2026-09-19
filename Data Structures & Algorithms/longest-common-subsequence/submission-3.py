dce54a3beff614ac0e1e000e8ff1d95dd8e80858
Nghia Vu <125529536+RupErz@users.noreply.github.com>
2026-09-19 09:56:22 -0500

    Add: longest-common-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        # text 1 = row, text 2 = col
+        topdown = [[0] * len(text2) for r in range(len(text1))]
+        ROWS, COLS = len(text1), len(text2)
+
+        # Start at the top we traverse down 
+        visited = {}
+        def dfs(r, c):
+            if r not in range(ROWS) or c not in range(COLS):
+                return 0
+            if (r, c) in visited:
+                return visited[(r, c)]
+            # Compare 2 character:
+            longest = 0
+            if text1[r] == text2[c]:
+                # Move diagonally 
+                longest = 1 + dfs(r + 1, c + 1)
+            else:
+                longest = max(dfs(r + 1, c), dfs(r, c + 1))
+            visited[(r, c)] = longest    
+
+            return longest 
+        
+        return dfs(0, 0)
